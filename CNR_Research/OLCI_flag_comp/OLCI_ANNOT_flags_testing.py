5f100dd3621a69c7e7fb828ec7596ffa0dda340e
Javier Concha <jaconcha@gmail.com>
2019-03-14 18:32:26 +0100

    Created histagrams

--- CNR_Research/OLCI_flag_comp/OLCI_ANNOT_flags_testing.py
+++ CNR_Research/OLCI_flag_comp/OLCI_ANNOT_flags_testing.py
@@ -29,7 +29,7 @@ chl2 = var2['chl'][250:1300,1200:2200]
 # chl1 = chl1
 
 plt.figure(figsize=(15,15))
-plt.subplot(1, 2, 1)
+plt.subplot(2, 2, 1)
 current_cmap = plt.cm.get_cmap()
 current_cmap.set_bad(color='white')
 plt.imshow(chl1,vmin=0, vmax=0.1) 
@@ -37,7 +37,7 @@ plt.title('w/o ANNOT flags')
 plt.colorbar(fraction=0.046, pad=0.04)
 # plt.ylabel('Damped oscillation')
 
-plt.subplot(1, 2, 2)
+plt.subplot(2, 2, 2)
 current_cmap = plt.cm.get_cmap()
 current_cmap.set_bad(color='white')
 plt.imshow(chl2,vmin=0, vmax=0.1)
@@ -46,30 +46,41 @@ plt.colorbar(fraction=0.046, pad=0.04)
 # plt.xlabel('time (s)')
 # plt.ylabel('Undamped')
 
+mask1_valid = ~chl1.mask
+mask2_valid = ~chl2.mask
+mask_diff = (mask1_valid ^ mask2_valid)
+
+plt.subplot(2, 2, 3)
+plt.imshow(mask_diff) 
+
+plt.subplot(2, 2, 4)
+urrent_cmap = plt.cm.get_cmap()
+current_cmap.set_bad(color='white')
+plt.imshow(chl1[mask_diff]) 
+
 plt.show()
+
+#type(chl1.mask)
 #%%
 # w/o ANNOT flags
 print(np.amax(chl1))
 print(np.amin(chl1))
 print(type(chl1))
 
-plt.figure(figsize=(15,15))
-plt.subplot(1, 2, 1)
-plt.hist(chl1[~chl1.mask], bins='auto',edgecolor='none',alpha=0.5) 
+plt.figure(figsize=(8,6))
+kwargs = dict(bins='auto',histtype='stepfilled', alpha=0.3)
+
+plt.hist(chl1[~chl1.mask], **kwargs) 
 plt.xscale('log')
-plt.title('w/o ANNOT flags')
-plt.xlabel('chl')
-plt.ylabel('Frequency')
 
 # w/ ANNOT flags
 print(np.amax(chl2))
 print(np.amin(chl2))
 print(type(chl2))
 
-plt.subplot(1, 2, 2)
-plt.hist(chl2[~chl2.mask], bins='auto',edgecolor='none',alpha=0.5) 
+#plt.subplot(1, 2, 2)
+plt.hist(chl2[~chl2.mask], **kwargs) 
 plt.xscale('log')
-plt.title('w/ ANNOT flags')
 plt.xlabel('chl')
 plt.ylabel('Frequency')
 
