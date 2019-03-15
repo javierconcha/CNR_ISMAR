25a2a30810be8487e5e8d9c7c877f0462720197f
Javier Concha <jaconcha@gmail.com>
2019-03-15 15:03:31 +0100

    working but w/o lat lon projection

--- CNR_Research/OLCI_flag_comp/OLCI_ANNOT_flags_testing.py
+++ CNR_Research/OLCI_flag_comp/OLCI_ANNOT_flags_testing.py
@@ -7,9 +7,7 @@ Created on Thu Mar 14 14:51:48 2019
 """
 #%%
 import numpy as np
-import numpy.ma as ma
 from netCDF4 import Dataset
-import sys, os
 from matplotlib import pyplot as plt
 
 #%%
@@ -23,24 +21,30 @@ nc_f2=Dataset(path+filename2, 'r')
 var1=nc_f1.variables
 var2=nc_f2.variables
 
-chl1 = var1['chl'][250:1300,1200:2200]
-chl2 = var2['chl'][250:1300,1200:2200]
+#chl1 = var1['chl'][250:1300,1200:2200]
+#chl2 = var2['chl'][250:1300,1200:2200]
+
+chl1 = var1['chl'][:,:]
+chl2 = var2['chl'][:,:]
+
+lat1 = var1['lat']
+lat2 = var2['lat']
 #%%
 # chl1 = chl1
 
-plt.figure(figsize=(15,15))
-plt.subplot(2, 2, 1)
+plt.figure(figsize=(12,12))
+plt.subplot(3, 2, 1)
 current_cmap = plt.cm.get_cmap()
 current_cmap.set_bad(color='white')
-plt.imshow(chl1,vmin=0, vmax=0.1) 
+plt.imshow(chl1,vmin=0, vmax=0.1, cmap='rainbow') 
 plt.title('w/o ANNOT flags')
 plt.colorbar(fraction=0.046, pad=0.04)
 # plt.ylabel('Damped oscillation')
 
-plt.subplot(2, 2, 2)
+plt.subplot(3, 2, 2)
 current_cmap = plt.cm.get_cmap()
 current_cmap.set_bad(color='white')
-plt.imshow(chl2,vmin=0, vmax=0.1)
+plt.imshow(chl2,vmin=0, vmax=0.1, cmap='rainbow')
 plt.title('w/ ANNOT flags')
 plt.colorbar(fraction=0.046, pad=0.04)
 # plt.xlabel('time (s)')
@@ -49,39 +53,49 @@ plt.colorbar(fraction=0.046, pad=0.04)
 mask1_valid = ~chl1.mask
 mask2_valid = ~chl2.mask
 mask_diff = (mask1_valid ^ mask2_valid)
+chl1_masked = np.ma.masked_where(~mask_diff,chl1)
 
-plt.subplot(2, 2, 3)
-plt.imshow(mask_diff) 
 
-plt.subplot(2, 2, 4)
-urrent_cmap = plt.cm.get_cmap()
+plt.subplot(3, 2, 3)
+plt.imshow(mask_diff, cmap='gray') 
+
+plt.subplot(3, 2, 4)
+current_cmap = plt.cm.get_cmap()
 current_cmap.set_bad(color='white')
-plt.imshow(chl1[mask_diff]) 
+plt.imshow(chl1_masked,vmin=0, vmax=0.1, cmap='rainbow') 
+plt.colorbar(fraction=0.046, pad=0.04)
 
-plt.show()
-
-#type(chl1.mask)
-#%%
-# w/o ANNOT flags
-print(np.amax(chl1))
-print(np.amin(chl1))
-print(type(chl1))
-
-plt.figure(figsize=(8,6))
+plt.subplot(3, 2, 5)
 kwargs = dict(bins='auto',histtype='stepfilled', alpha=0.3)
 
 plt.hist(chl1[~chl1.mask], **kwargs) 
 plt.xscale('log')
 
-# w/ ANNOT flags
-print(np.amax(chl2))
-print(np.amin(chl2))
-print(type(chl2))
-
-#plt.subplot(1, 2, 2)
 plt.hist(chl2[~chl2.mask], **kwargs) 
 plt.xscale('log')
 plt.xlabel('chl')
 plt.ylabel('Frequency')
 
 plt.show()
+
+#%%
+# w/o ANNOT flags
+print(np.nanmin(chl1[~chl1.mask]))
+print(np.nanmax(chl1[~chl1.mask]))
+print(np.nanstd(chl1[~chl1.mask]))
+print(np.nanmedian(chl1[~chl1.mask]))
+print(np.nanmean(chl1[~chl1.mask]))
+print(sum(sum(~chl1.mask)))
+print(type(chl1))
+
+# w/ ANNOT flags
+print(np.nanmin(chl2[~chl2.mask]))
+print(np.nanmax(chl2[~chl2.mask]))
+print(np.nanstd(chl2[~chl2.mask]))
+print(np.nanmedian(chl2[~chl2.mask]))
+print(np.nanmean(chl2[~chl2.mask]))
+print(type(chl2))
+print(sum(sum(~chl2.mask)))
+print('Pixel Difference:\n')
+print(sum(sum(~chl1.mask))-sum(sum(~chl2.mask)))
+print(sum(sum(~chl1_masked.mask)))
