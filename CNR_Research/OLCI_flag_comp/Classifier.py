9db9e3a270fc6fecdff9784a64eef61a7c98d598
Javier Concha <jaconcha@gmail.com>
2019-05-03 13:36:00 +0200

    Calculate Mahalonobis distance and classify Case 1 or Case 2 or a blending (mixed).

--- CNR_Research/OLCI_flag_comp/Classifier.py
+++ CNR_Research/OLCI_flag_comp/Classifier.py
@@ -6,6 +6,12 @@ Created on Fri Apr 12 11:19:18 2019
 @author: javier
 
 from computemdistance.pro and make_chl_mario.pro by Mario
+This classifier is based on D'Alimonte et al. (2003): "Use of the Novelty Detection Technique to
+Identify the Range of Applicability of Empirical Ocean Color Algorithms"
+
+Inputs:
+    - CovMatAAOT_245.nc
+    - CovMatMEDOC4_245.nc
 """
 #%% Initializaion
 import numpy as np
@@ -17,9 +23,13 @@ from mpl_toolkits.basemap import Basemap
 from numpy.linalg import inv
 from numpy.linalg import det
 
+import math
+
 import os.path
 import os
 os.environ['QT_QPA_PLATFORM']='offscreen' # to avoid error "QXcbConnection: Could not connect to display"
+
+import time
 
 from os import access, R_OK
 #%%
@@ -96,11 +106,13 @@ filename2 = 'CovMatMEDOC4_245.nc'
 nc_f1=Dataset(os.path.join(path1,filename1), 'r')
 nc_f2=Dataset(os.path.join(path1,filename2), 'r')
 
+# Case 2
 CovMatAAOT_245_MATRIX = nc_f1.variables['MATRIX'][:]
 CovMatAAOT_245_INVERSE = nc_f1.variables['INVERSE'][:]
 CovMatAAOT_245_DETERMINANT = nc_f1.variables['DETERMINANT'][:]
 CovMatAAOT_245_MEAN = nc_f1.variables['MEAN'][:]
 
+# Case 1
 CovMatMEDOC4_245_MATRIX = nc_f2.variables['MATRIX'][:]
 CovMatMEDOC4_245_INVERSE = nc_f2.variables['INVERSE'][:]
 CovMatMEDOC4_245_DETERMINANT = nc_f2.variables['DETERMINANT'][:]
@@ -124,17 +136,65 @@ case2=10**(a2[0]+a2[1]*r2+a2[2]*r2**2+a2[3]*r2**3) # Berthon & Zibordi (2003) IJ
 spectrum=np.log10(rrs[[0,2,3]]*F0[[0,2,3]])
 #%%
 #; Case 1
-d1 = computemdistance(CovMatAAOT_245_MATRIX,CovMatAAOT_245_MEAN,spectrum)
-
-
-
+d1 = computemdistance(CovMatMEDOC4_245_MATRIX,CovMatMEDOC4_245_MEAN,spectrum)
 print(d1)
+p1=1/((2*math.pi)**(len(spectrum)/2.)*math.sqrt(CovMatMEDOC4_245_DETERMINANT))*math.exp(-0.5*d1) # pdf for case 1
+print(p1)
 
-#p1=1/((2*DOUBLE(!PI))^(N_ELEMENTS(spectrum)/2.)*SQRT(case1dataset.determinant))*EXP(-0.5*d1)
+
 #; Case 2
-#d2=COMPUTEMDISTANCE(case2dataset,spectrum)
-#p2=1/((2*DOUBLE(!PI))^(N_ELEMENTS(spectrum)/2.)*SQRT(case2dataset.determinant))*EXP(-0.5*d2)
-#%%
+d2 = computemdistance(CovMatAAOT_245_MATRIX,CovMatAAOT_245_MEAN,spectrum)
+print(d2)
+p2=1/((2*math.pi)**(len(spectrum)/2.)*math.sqrt(CovMatAAOT_245_DETERMINANT))*math.exp(-0.5*d2) # pdf for case 2
+print(p2)
+
+#;-------------------------------------
+#; Merged Chl is compute with one of following case
+#;-------------------------------------
+#; Case 1
+if d1 <= 6.25 and d2 > 6.25:
+    print('Case 1')
+    out = case1
+    wtm = 1
+#; Case 2    
+elif d1 > 6.25 and d2 <= 6.25:
+    print('Case 2')
+    out = case2
+    wtm = 0
+#; Mix of the two (blending approach)
+else:
+    print('Mixed')
+    out = (p1*case1+p2*case2)/(p1+p2)
+    wtm = p1/(p1+p2)
+    
+print(out)
+print(wtm)
+
+if math.isnan(out):
+#    localtime = time.asctime( time.localtime(time.time()) )
+#    print(localtime+" make_merged_chl - INFO - Pixel ("+"column_line"+ ") was found NaN and set to missing value (-999)")
+    out = -999
+
+# From make_chl_mario.pro:
+#    d1 LE 6.25 AND d2 GT 6.25: BEGIN & out(igood)=case1 & wtm(igood)=1 & END
+#; Case 2
+#    d1 GT 6.25 AND d2 LE 6.25: BEGIN & out(igood)=case2 & wtm(igood)=0 & END
+#; Mix of the two
+#    ELSE: BEGIN & out(igood)=(p1*case1+p2*case2)/(p1+p2) & wtm(igood)=p1/(p1+p2) & END
+#    ENDCASE
+#    IF FINITE(out(igood)) EQ 0 THEN BEGIN
+#       PRINT,STRING(SYSTIME())+" make_merged_chl - INFO - Pixel ("+STRING(column_line(sea(good(igood)),dim(0),/SILENT),FORMAT='(2(i4,x))')+") was found NaN and set to missing value (-999)"
+#       out(igood)=-999
+#    ENDIF
+#
+#ENDFOR
+#
+#chl=FLTARR(dim)-999. & chl(sea(good))=out
+#mas=FLTARR(dim)-999. & mas(sea(good))=wtm
+#
+#RETURN,CREATE_STRUCT('CHL',chl,'WTM',mas)
+
+#%% Example: https://jamesmccaffrey.wordpress.com/2017/11/09/example-of-calculating-the-mahalanobis-distance/
 XYZ = np.matrix([[64, 580, 29],[66,570,33],[68,590,37],[69,660,46],[73,600,55]])
 m = np.array([68,600,40])
 cov_mat = np.cov(np.transpose(XYZ))
@@ -143,5 +203,7 @@ n = 5
 v = np.array([66,640,44])
 
 d3 = computemdistance(cov_mat,m,v)
+MD3 = math.sqrt(d3)
 
 print(d3)
+print(MD3)
