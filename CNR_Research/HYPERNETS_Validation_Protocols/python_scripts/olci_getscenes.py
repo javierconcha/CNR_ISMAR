7f0d650ab304d3aec90fd6a0d665536a0ed5fef4
Javier A. Concha <jaconcha@gmail.com>
2019-09-02 16:44:23 +0200

    Call to dhusget.sh working properly.

--- CNR_Research/HYPERNETS_Validation_Protocols/python_scripts/olci_getscenes.py
+++ CNR_Research/HYPERNETS_Validation_Protocols/python_scripts/olci_getscenes.py
@@ -35,60 +35,62 @@ main_path = '/Users/javier.concha/Desktop/Javier/2019_Roma/CNR_Research/HYPERNET
 #%%
 def olci_get(date,lat1,lat2,lon1,lon2):
     
-    
-    if (date-datetime.datetime(2017,11,29)).total_seconds()<0:
-        site="codarep.eumetsat.int"
-    else:
-        site="coda.eumetsat.int"
-    year=str(date.year)
-    month='0'*(2-len(str(date.month)))+str(date.month)    
-    day='0'*(2-len(str(date.day)))+str(date.day)    
-    datestart = year+'-'+month+'-'+day
-    dateend = datestart
-    
-    lat1_str = '{:,.2f}'.format(lat1)
-    lat2_str = '{:,.2f}'.format(lat2)
-    lon1_str = '{:,.2f}'.format(lon1)
-    lon2_str = '{:,.2f}'.format(lon2)
-    location_str = lon1_str+','+lat1_str+':'+lon2_str+','+lat2_str #"12.0,44.0:14.0,46.00"
-    print(location_str)
-    
-    csv_f = main_path+'csv_file/'+datestart.replace('-','')+"_"+dateend.replace('-','')+'.csv'
-    
-    print('csv_f:')
-    print(csv_f)
-    
-    os.system('rm -r /Users/javier.concha/dhusget_tmp/')
-    
-    cmd = './dhusget.sh -u jaconcha -p a1b2c3d4 '+\
-    '-d '+site+' -m Sentinel-3 -i OLCI -T OL_2_WFR___ '+\
-    '-S '+datestart+'T00:00:00Z -E '+dateend+'T23:59:59Z '+\
-    '-c '+location_str+' '\
-    "-F "+"'timeliness:Non Time Critical' "\
-    '-C '+csv_f
-#    '-L /Users/javier.concha/dhusget_tmp/'
-    print(cmd)
-    
-    os.system(cmd)
-
     file_list = []
-#    i = 0
-    with open(csv_f) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            if row[0][0:3] == 'S3A':
-                file_list.append(row[0])
-                
     
-#            if i==0 or i%2==0:
-#                file_olci=row[0]
-#                file_list.append(file_olci)
-#                i+=1
-#    print (file_list)
-#    os.system('rm '+csv_f)
-    os.system('rm -r /Users/javier.concha/dhusget_tmp/')
+    if (date-datetime.datetime(2016,4,26)).total_seconds()>=0:
+        if (date-datetime.datetime(2017,11,29)).total_seconds()<=0:
+            site="codarep.eumetsat.int"
+        else:
+            site="coda.eumetsat.int"
+        year=str(date.year)
+        month='0'*(2-len(str(date.month)))+str(date.month)    
+        day='0'*(2-len(str(date.day)))+str(date.day)    
+        datestart = year+'-'+month+'-'+day
+        dateend = datestart
+        
+        lat1_str = '{:,.2f}'.format(lat1)
+        lat2_str = '{:,.2f}'.format(lat2)
+        lon1_str = '{:,.2f}'.format(lon1)
+        lon2_str = '{:,.2f}'.format(lon2)
+        location_str = lon1_str+','+lat1_str+':'+lon2_str+','+lat2_str #"12.0,44.0:14.0,46.00"
+        print(location_str)
+        
+        csv_f = main_path+'csv_file/'+datestart.replace('-','')+"_"+dateend.replace('-','')+'.csv'
+        
+        print('csv_f:')
+        print(csv_f)
+        
+#        os.system('rm -r /Users/javier.concha/dhusget_tmp/')
+        
+        cmd = './dhusget.sh -u jaconcha -p a1b2c3d4 '+\
+        '-d '+site+' -m Sentinel-3 -i OLCI -T OL_2_WFR___ '+\
+        '-S '+datestart+'T00:00:00Z -E '+dateend+'T23:59:59Z '+\
+        '-c '+location_str+' '+\
+        '-F '+"'timeliness:"+'"Non Time Critical"'+"' "+\
+        '-L /Users/javier.concha/dhusget_tmp1/ '+\
+        '-C '+csv_f
+        
+        print(cmd)
+        
+        os.system(cmd)
     
-    
+    #    i = 0
+        with open(csv_f) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                if row[0][0:3] == 'S3A':
+                    file_list.append(row[0])
+                    
+        
+    #            if i==0 or i%2==0:
+    #                file_olci=row[0]
+    #                file_list.append(file_olci)
+    #                i+=1
+    #    print (file_list)
+    #    os.system('rm '+csv_f)
+        os.system('rm -r /Users/javier.concha/dhusget_tmp/')
+        
+        
 
     return file_list    
 
